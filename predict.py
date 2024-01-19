71b714db729418c87524a61c8f72c1103c14bf5c
Max <muzeaumax@gmail.com>
2024-01-19 10:14:22 +0100

    maj 19/01/24

--- predict.py
+++ predict.py
@@ -12,14 +12,14 @@ if __name__ == "__main__":
     params = {}
     params['type'] = 'generator' # 'predictor' to reconstruct images and 'generator' to generate patches only
     params['device'] = 'cuda'
-    params['save_dir'] = './data/res_pred_test'
+    params['save_dir'] = './data/train_results'
     params['weights_dir'] = './pipeline/out/L_rx_0.04'
     params['norm'] = np.array([[-1,-1,-1,-1],[10.5,10.5,10.5,10.5]]) # matrix of size (2,c) c being the number of channels
     params['channels'] = 4 # 1 for SLC and 4 for full pol
     params['pat_size'] = 32
     params['stride'] = 16
     params['z_size'] = 128 # lattent space size
-    params['eval_dir'] = '/media/max/TOSHIBA EXT/ONERA_SONDRA/data/under/Gretsi 2k23/eval_patches/denoised'
+    params['eval_dir'] = './data/eval'
     params['batch_size'] = 1 # images of different sizes
     params['print_freq'] = 1 # Print in the logger for each image
     params['semi_kernel_sz'] = 5 # size of the semi kernel to compute scm
